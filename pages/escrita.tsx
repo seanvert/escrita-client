9f963d618bd981fda362bda7512dac5f9a6ee9b5
Sean Uchida <seankuchida@gmail.com>
2022-10-02 12:15:01 -0300

    escrita commit inicial

---
+++ pages/escrita.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Atividade from "../lib/Atividades"
+import Main from "../layouts/Main";
+
+const Escrita: React.FC = (props) => {
+    return (
+        <Main title="Atividade">
+                <Atividade />
+        </Main>
+        
+    );
+}
+
+export default Escrita;
