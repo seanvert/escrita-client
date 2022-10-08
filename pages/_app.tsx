837fb2db2005f64e99ac8cc7012c08c5413c8f3e
Sean Uchida <seankuchida@gmail.com>
2022-10-08 14:44:33 -0300

    json parse

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { initializeApp } from 'firebase-admin'
 
-initializeApp(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const GOOGLE_APPLICATION_CREDENTIALS = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+initializeApp(GOOGLE_APPLICATION_CREDENTIALS);
 
 // TODO aqui que vai o contexto
 function MyApp({ Component, pageProps }: AppProps) {
