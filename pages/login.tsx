a046ac235bbe26a57df7feadbbb0fb5ee829d518
Sean Uchida <seankuchida@gmail.com>
2022-10-11 10:39:51 -0300

    layout login pronto

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,28 +4,36 @@ import Main from "../layouts/Main";
 const Login: React.FC = (props) => {
 	return (
 		<Main title="Login">
-			<div className="grow">
-				<form className="font-bold"
-					  action="/send-data-here" method="post">
-					<label>Usuário:</label>
-					<input
-					className="my-2"
-					type="text"
-					id="user" name="usuario"
-					required
-					/>
-					<label>Senha:</label>
-					<input
-					className="my-2"
-					type="password"
-					id="last" name="senha"
-					required
-					/>
-					<button className="bg-orange-300 p-1 rounded"
-						type="submit">
-						Enviar
-					</button>
-				</form>
+			<div className="grow grid">
+				<div className="self-center text-center">
+					<form className="font-bold bg-orange-800"
+						  action="/send-data-here" method="post">
+						<div className="flex-col">
+							<div>
+								<label>Usuário:</label>
+								<input
+								className="my-2"
+								type="text"
+								id="user" name="usuario"
+								required
+								/>
+							</div>
+							<div>
+								<label>Senha:</label>
+								<input
+								className="my-2"
+								type="password"
+								id="last" name="senha"
+								required
+								/>
+							</div>
+							<button className="bg-orange-300 p-1 rounded"
+								type="submit">
+								Enviar
+							</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</Main>
 	);
